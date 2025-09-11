659ba90926bb9fb32dd8eb18bb5c43e4ad1c9f92
Ankit <ankit26@gmail.com>
2025-09-11 17:08:06 +0530

    auth implmented

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,22 +1,40 @@
 import express from "express";
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
+import {ClerkMiddleware} from "@clerk/clerk-sdk-node";
+import { functions, inngest } from "./config/inngest.js";
+import { serve } from "inngest/express";
 
 
 
 const app = express();
+app.use(express.json());
+app.use(ClerkMiddleware());
 
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 
 app.get("/", (req, res) => {
-    res.send("Hello World")
+    res.send("Hello World! 123")
 });
 
 
 
-app.listen(ENV.PORT, () => {
+const startServer = async() => {
+    try {
+        await connectDB();
+        if(ENV.NODE_ENV !== "production"){
+            app.listen(ENV.PORT, () => {
     console.log("Server started at port:", ENV.PORT)
     connectDB();
-});
+         });
 
+    }
+    } catch (error) {
+        console.error("Error starting server:", error);
+        process.exit(1);
+    }
+};
+startServer();
+export default app;
 
